213df2441b577711f378522eb2e7f59349b3a466
Shubham Patil <Shubham.Patil@ingrammicro.com>
2024-06-19 19:49:24 +0530

    Adding MUI Adding AppBar Component Adding Footer Component

--- e-commerce-amazon-clone/src/App.tsx
+++ e-commerce-amazon-clone/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/headerComponents/navBarComponent';
+import StickyFooter from './components/footerComponents/footerComponent';
 
 function App() {
   return (
     <>
+    <NavBar/>
+    <StickyFooter/>
     </>  );
 }
 
